f7c5523138c741edfa6bb694b665ed1bc0c1c31d
notfelineit <notfelineit@gmail.com>
2018-05-02 00:01:25 -0700

    Add back LEDS

--- printer.js
+++ printer.js
@@ -10,38 +10,38 @@ var SerialPort = require('serialport'),
 var path = __dirname + '/images/boh_small.png'
 var printer, numMessages
 var initialDataLoaded = true
-// var Gpio = require('onoff').Gpio //include onoff to interact with the GPIO
-// var LED1 = new Gpio(21, 'out') //display red
-// var LED2 = new Gpio(26, 'out') //display red
-// var LED3 = new Gpio(20, 'out') //blinking red button
-// var LED4 = new Gpio(19, 'out') //blinking blue button
-// var LED5 = new Gpio(13, 'out') //blinking blue button
-//var blinkInterval = setInterval(blinkLED, 200) //run the blinkLED function every 250ms
-//var blinkInterval2 = setInterval(blinkLED2, 450)
+var Gpio = require('onoff').Gpio //include onoff to interact with the GPIO
+var LED1 = new Gpio(21, 'out') //display red
+var LED2 = new Gpio(26, 'out') //display red
+var LED3 = new Gpio(20, 'out') //blinking red button
+var LED4 = new Gpio(19, 'out') //blinking blue button
+var LED5 = new Gpio(13, 'out') //blinking blue button
+var blinkInterval = setInterval(blinkLED, 200) //run the blinkLED function every 250ms
+var blinkInterval2 = setInterval(blinkLED2, 450)
 
-// function blinkLED() {
-//   //function to start blinking
-//   if (LED4.readSync() === 0) {
-//     //check the pin state, if the state is 0 (or off)
-//     LED4.writeSync(1) //set pin state to 1 (turn LED on)
-//   } else {
-//     LED4.writeSync(0) //set pin state to 0 (turn LED off)
-//   }
-// }
-// function blinkLED2() {
-//   //function to start blinking
-//   if (LED5.readSync() === 0) {
-//     //check the pin state, if the state is 0 (or off)
-//     LED5.writeSync(1) //set pin state to 1 (turn LED on)
-//   } else {
-//     LED5.writeSync(0) //set pin state to 0 (turn LED off)
-//   }
-// }
+function blinkLED() {
+  //function to start blinking
+  if (LED4.readSync() === 0) {
+    //check the pin state, if the state is 0 (or off)
+    LED4.writeSync(1) //set pin state to 1 (turn LED on)
+  } else {
+    LED4.writeSync(0) //set pin state to 0 (turn LED off)
+  }
+}
+function blinkLED2() {
+  //function to start blinking
+  if (LED5.readSync() === 0) {
+    //check the pin state, if the state is 0 (or off)
+    LED5.writeSync(1) //set pin state to 1 (turn LED on)
+  } else {
+    LED5.writeSync(0) //set pin state to 0 (turn LED off)
+  }
+}
 
 function processMessage(dataKey, dataValue) {
   if (printer) {
-    // LED1.writeSync(1)
-    // LED2.writeSync(1)
+    LED1.writeSync(1)
+    LED2.writeSync(1)
     printer
       .printLine('')
       .printLine('')
@@ -68,7 +68,7 @@ function processMessage(dataKey, dataValue) {
       .printLine('')
       .printLine('')
       .print()
-    //initializeBarGraph()
+    initializeBarGraph()
   }
   console.log('data key: ', dataKey)
   console.log('data value: ', dataValue)
@@ -76,38 +76,38 @@ function processMessage(dataKey, dataValue) {
   //console.log('TEST number of messages in bank:', numMessagestest)
 }
 
-// function blink() {
-//   function turnOff() {
-//     //function to stop blinking
-//     LED1.writeSync(0) // Turn LED off
-//     LED2.writeSync(0)
-//     LED1.unexport() // Unexport GPIO to free resources
-//     LED2.unexport() // Unexport GPIO to free resources
-//   }
-//
-//   setTimeout(turnOff, 25000)
-//
-//   var blinkInterval3 = setInterval(blinkLED3, 500)
-//
-//   function blinkLED3() {
-//     //function to start blinking
-//     if (LED3.readSync() === 0) {
-//       //check the pin state, if the state is 0 (or off)
-//       LED3.writeSync(1) //set pin state to 1 (turn LED on)
-//     } else {
-//       LED3.writeSync(0) //set pin state to 0 (turn LED off)
-//     }
-//   }
-//
-//   function endBlink3() {
-//     //function to stop blinking
-//     clearInterval(blinkInterval3) // Stop blink intervals
-//     LED3.writeSync(0) // Turn LED off
-//     LED3.unexport() // Unexport GPIO to free resources
-//   }
-//
-//   setTimeout(endBlink3, 20000) //stop blinking after 5 seconds
-// }
+function blink() {
+  function turnOff() {
+    //function to stop blinking
+    LED1.writeSync(0) // Turn LED off
+    LED2.writeSync(0)
+    LED1.unexport() // Unexport GPIO to free resources
+    LED2.unexport() // Unexport GPIO to free resources
+  }
+
+  setTimeout(turnOff, 25000)
+
+  var blinkInterval3 = setInterval(blinkLED3, 500)
+
+  function blinkLED3() {
+    //function to start blinking
+    if (LED3.readSync() === 0) {
+      //check the pin state, if the state is 0 (or off)
+      LED3.writeSync(1) //set pin state to 1 (turn LED on)
+    } else {
+      LED3.writeSync(0) //set pin state to 0 (turn LED off)
+    }
+  }
+
+  function endBlink3() {
+    //function to stop blinking
+    clearInterval(blinkInterval3) // Stop blink intervals
+    LED3.writeSync(0) // Turn LED off
+    LED3.unexport() // Unexport GPIO to free resources
+  }
+
+  setTimeout(endBlink3, 20000) //stop blinking after 5 seconds
+}
 
 function initializeBarGraph() {
   //needs to be continuously updating
@@ -193,4 +193,4 @@ serialPort.on('error', () => {
   serialPort.flush(() => console.log('Serial port flushed.'))
 })
 
-//blink()
+blink()
